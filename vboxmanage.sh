901d5a7907a1d1b02e214e29b0e9cbe545d94fad
Brent De Clercq <brent.declercq2@student.hogent.be>
2024-04-11 19:29:29 +0200

    vboxmanage.sh aangepast

--- vboxmanage.sh
+++ vboxmanage.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
 # Vars
 VM_NAME="Debian-Cyber-NPE"
-VM_IP=192.168.53.100
+VM_IP="192.168.53.100"
 
 # Delete existing VM
+VBoxManage controlvm $VM_NAME poweroff
 VBoxManage unregistervm $VM_NAME --delete
+
+# Copy vdi file
+cp ./debian/server.vdi ./osboxes.vdi
+
 
 # Create VM
 VBoxManage createvm --name "$VM_NAME" --ostype "Debian_64" --register
 VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ./osboxes.vdi
+VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium osboxes.vdi
 VBoxManage modifyvm "$VM_NAME" --memory 1024 --vram 128
 VBoxManage modifyvm "$VM_NAME" --nic1 nat
-VBoxManage modifyvm "$VM_NAME" --nic2 intnet --intnet2 "cyber-npe"
+VBoxManage modifyvm "$VM_NAME" --nic2 hostonly --hostonlyadapter2 vboxnet1
 VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
 # Set IP address on internal network
@@ -22,4 +27,8 @@ VBoxManage guestproperty set "$VM_NAME" "/VirtualBox/GuestInfo/Net/1/V4/IP" "$VM
 # Start VM
 VBoxManage startvm "$VM_NAME" --type headless
 
-sshpass -p 'osboxes.org' ssh osboxes@$VM_IP "bash -s" < install.sh
+# Make network work
+VBoxManage controlvm $VM_NAME poweroff
+VBoxManage startvm $VM_NAME --type headless
+
+# sshpass -p 'osboxes.org' ssh osboxes@$VM_IP "bash -s" < install.sh
