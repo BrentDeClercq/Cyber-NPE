97b439b5012cc2afc04fee37c00d3b3aebfe3961
Brent De Clercq <brent.declercq2@student.hogent.be>
2024-04-15 13:15:33 +0200

    Prutsen

--- vboxmanage.sh
+++ vboxmanage.sh
@@ -2,7 +2,8 @@
 
 # Vars
 VM_NAME="Debian-Cyber-NPE"
-VM_IP="192.168.53.100"
+USERNAME="osboxes"
+PASSWORD="osboxes.org"
 
 # Delete existing VM
 VBoxManage controlvm $VM_NAME poweroff
@@ -21,14 +22,12 @@ VBoxManage modifyvm "$VM_NAME" --nic1 nat
 VBoxManage modifyvm "$VM_NAME" --nic2 hostonly --hostonlyadapter2 vboxnet1
 VBoxManage modifyvm "$VM_NAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none
 
-# Set IP address on internal network
-VBoxManage guestproperty set "$VM_NAME" "/VirtualBox/GuestInfo/Net/1/V4/IP" "$VM_IP"
-
 # Start VM
 VBoxManage startvm "$VM_NAME" --type headless
 
-# Make network work
-VBoxManage controlvm $VM_NAME poweroff
-VBoxManage startvm $VM_NAME --type headless
+# Copy script to VM
 
-# sshpass -p 'osboxes.org' ssh osboxes@$VM_IP "bash -s" < install.sh
+VBoxManage guestcontrol "$VM_NAME" copyto --target-directory=/home/osboxes/ --username=$USERNAME --password=$PASSWORD install.sh
+
+
+# sshpass -p 'osboxes.org' ssh osboxes@$VM_IP "bash -s" < install.sh    
